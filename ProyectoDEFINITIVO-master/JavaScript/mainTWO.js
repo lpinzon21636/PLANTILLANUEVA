642c7fde1cc7dc39a9ed52dcde87410dfeadb5b3
lpinzon21636 <35942551+lpinzon21636@users.noreply.github.com>
2019-04-01 21:49:12 -0500

    Update mainTWO.js

--- ProyectoDEFINITIVO-master/JavaScript/mainTWO.js
+++ ProyectoDEFINITIVO-master/JavaScript/mainTWO.js
@@ -2,7 +2,7 @@
 * Conexión con la imagen para cambiar el botón al momento de ser
 * oprimido
 */
-var interruptor= document.getElementById('interruptor');
+var Interruptor= document.getElementById('interruptor');
 
 estado = document.getElementById('estado');
 
