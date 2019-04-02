b9c251ef3df9f06dae9a1cd8a2bfd87a95568d4f
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-04-01 21:11:03 -0500

    Prueba MOCKUP botón encendido/apagado

---
+++ ProyectoDEFINITIVO-master/JavaScript/mainTWO.js
@@ -0,0 +1,21 @@
+/**
+* Conexión con la imagen para cambiar el botón al momento de ser
+* oprimido
+*/
+var interruptor= document.getElementById('interruptor');
+
+estado = document.getElementById('estado');
+
+function cambioEstado() {
+    if (interruptor.getAttribute('class')==='apagado'){
+        interruptor.setAttribute('class', 'encendido');
+        console.log("LUCES ENCENDIDAS");
+        estado.innerHTML="LUCES ENCENDIDAS";
+    } else {
+        interruptor.setAttribute('class', 'apagado');
+        console.log("LUCES APAGADAS");
+        estado.innerHTML="LUCES APAGADAS";
+    }
+}
+
+interruptor.addEventListener('click',cambioEstado);
