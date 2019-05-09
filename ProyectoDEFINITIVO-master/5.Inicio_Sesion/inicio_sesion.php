731ba8ca3e5fcb403ec19e49a2b041241ee89536
lpinzon21636 <35942551+lpinzon21636@users.noreply.github.com>
2019-05-08 22:57:26 -0500

    Update inicio_sesion.php

--- ProyectoDEFINITIVO-master/5.Inicio_Sesion/inicio_sesion.php
+++ ProyectoDEFINITIVO-master/5.Inicio_Sesion/inicio_sesion.php
@@ -56,7 +56,7 @@
     <label for="exampleInputPassword1">Contraseña</label>
     <input type="password" class="form-control" id="contrasena" name="contrasena" placeholder="Password">
   </div>
-  <button type="submit" class="btn btn-primary" value="send">Ingresar</button>
+  <button type="submit" class="btn btn-primary" value="send">Ingresaar</button>
 </form>
 
     <script src= "https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity= "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin= "anonymous" ></script>
