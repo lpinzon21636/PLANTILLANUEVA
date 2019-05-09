6ce825a230366abf6df4b31b4e34a9abbaf3365a
Natalia Peñaranda <nataliapenaranda@MacBook-Air-de-Natalia.local>
2019-05-08 21:09:25 -0500

    mod equipo de trabajo

--- ProyectoDEFINITIVO-master/3.teamwork/teamwork.php
+++ ProyectoDEFINITIVO-master/3.teamwork/teamwork.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   <!--
     <meta charset="UTF-8">
     <meta name= "viewport" content= "width=device-width, initial-scale=1, shrink-to-fit=no" >
     <link rel= "stylesheet" href= "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity= "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin= "anonymous" >
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <title>Equipo de trabajo</title>
 </head>
 <body style="background-color: RGB(101,132,184);">
