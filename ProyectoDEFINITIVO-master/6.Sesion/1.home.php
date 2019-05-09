6bd55b918a2869c2503dc0bf2a7c86b9f01b8d9f
Natalia Peñaranda <nataliapenaranda@MacBook-Air-de-Natalia.local>
2019-05-09 00:03:35 -0500

    modificacion de textos en cards

--- ProyectoDEFINITIVO-master/6.Sesion/1.home.php
+++ ProyectoDEFINITIVO-master/6.Sesion/1.home.php
@@ -47,7 +47,12 @@
 									<div class="card" style="width: 18rem; background: none;">
 						  				<img src="../Images/580b585b2edbce24c47b2798.png" class="card-img-top imagen-adaptativa5" alt="...">
 										<div class="card-body">
+<<<<<<< HEAD
 											<h5 class="card-title" >Card title</h5>
+=======
+											<h5 class="card-title">Bombillos</h5>
+											<p class="card-text">Desde aqui usted podrá encender y apagar las luces de la sala. Utilice el interruptor para realizar una acción sobre los bombillos.</p>
+>>>>>>> modificacion de textos en cards
 											<div align="center" class="custom-control custom-switch">
 												<input type="checkbox" class="custom-control-input" id="customSwitch2">
 												<label class="custom-control-label" for="customSwitch2"></label>
@@ -59,7 +64,12 @@
 									<div class="card" style="width: 18rem; background: none;">
 						  				<img src="../Images/2357c06358cde99.jpg" class="card-img-top imagen-adaptativa5" alt="...">
 										<div class="card-body">
+<<<<<<< HEAD
 											<h5 class="card-title">Card title</h5>
+=======
+											<h5 class="card-title">Puertas</h5>
+											<p class="card-text">Controle las puertas de la sala, abralas o cierrelas desde el interrumptor.</p>
+>>>>>>> modificacion de textos en cards
 											<div align="center" class="custom-control custom-switch">
 												<input type="checkbox" class="custom-control-input" id="customSwitch3">
 												<label class="custom-control-label" for="customSwitch3"></label>
@@ -71,7 +81,12 @@
 									<div class="card" style="width: 18rem; background: none;">
 						  				<img src="../Images/929b3e551fd485386d0fe0a908e03ac8-icono-de-l--mpara-de-mesa-by-vexels.png" class="card-img-top imagen-adaptativa5" alt="...">
 										<div class="card-body">
+<<<<<<< HEAD
 											<h5 class="card-title">Card title</h5>
+=======
+											<h5 class="card-title">Lamparas de mesa</h5>
+											<p class="card-text">Controle el estado de on/off de las luces de mesa que tenga en esta área.</p>
+>>>>>>> modificacion de textos en cards
 											<div align="center" class="custom-control custom-switch">
 												<input type="checkbox" class="custom-control-input" id="customSwitch4">
 												<label class="custom-control-label" for="customSwitch4"></label>
@@ -91,6 +106,7 @@
                             </button>
                         </h2>
                     </div>
+<<<<<<< HEAD
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample" style="background: none;">
                         <div class="card-body" style="background: none;">
                             <div class="row" style="background: none;">
@@ -128,6 +144,18 @@
 												<label class="custom-control-label" for="customSwitch7"></label>
 											</div>
 										</div>
+=======
+                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                        <div class="card-body">
+                            <div class="card" style="width: 18rem;">
+				  				<img src="../Images/580b585b2edbce24c47b2798.png" class="card-img-top imagen-adaptativa5" alt="...">
+								<div class="card-body">
+									<h5 class="card-title">Bombillos</h5>
+									<p class="card-text">Desde aqui usted podrá encender y apagar las luces de la sala. Utilice el interruptor para realizar una acción sobre los bombillos.</p>
+									<div align="center" class="custom-control custom-switch">
+										<input type="checkbox" class="custom-control-input" id="customSwitch5">
+										<label class="custom-control-label" for="customSwitch5"></label>
+>>>>>>> modificacion de textos en cards
 									</div>
 								</div>
 							</div>
