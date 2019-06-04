eff3d4ee74cfeec19c9579b5b4eca2a41ec9e75f
mperezs06408 <mperezs06408@universidadean.edu.co>
2019-06-03 19:18:54 -0500

    Prueba final inicio sesión con MYSQL

--- ProyectoDEFINITIVO-master/6.Sesion/sesion.php
+++ ProyectoDEFINITIVO-master/6.Sesion/sesion.php
@@ -2,17 +2,30 @@
 <?php
     if ($_POST['correo']== "" || $_POST['contrasena']== "") {
        echo '<script language="javascript">';
-        echo 'alert("Datos incorrectos o incompletos")';  //not showing an alert box.
+        echo 'alert("No pueden existir campos vacíos")';
         echo '</script>';
         echo '<script language="javascript">';
         echo 'window.location.href="../5.Inicio_Sesion/inicio_sesion.php";';
         echo '</script>';
     } else {
-        $sql="SELECT COUNT(*) FROM PERSONA WHERE Usuario='".$_POST['correo']."' AND Contrasena='".$_POST['contrasena']."'";
+        $sql="SELECT * FROM PERSONA WHERE Usuario='".$_POST['correo']."' AND Contrasena='".$_POST['contrasena']."'";
         echo $sql;
-        $result = mysql_query($sql,$conexion);
-        echo mysql_result($result,0,'COUNT');
-
+        $result = mysqli_query($conexion,$sql);
+        if (mysqli_num_rows($result) == 0) {
+            echo '<script language="javascript">';
+            echo 'alert("Usuario o contraseña incorrecta")';
+            echo '</script>';
+            echo '<script language="javascript">';
+            echo 'window.location.href="../5.Inicio_Sesion/inicio_sesion.php";';
+            echo '</script>';
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Bienvenido")';  //not showing an alert box.
+            echo '</script>';
+            echo '<script language="javascript">';
+            echo 'window.location.href="1.home.php";';
+            echo '</script>';
+        }
 
     }
 ?>
